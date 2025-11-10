1f34ae0954dfaf5b00ce8d586f13e1c28138c9cf
Cristian909 <cristian.zamfirescu@stud.acs.upb.ro>
2025-11-10 19:19:43 +0200

    I can not do this.

--- app/src/main/java/ro/pub/cs/systems/eim/practicaltest01var04/PracticalTest01Var04MainActivity.kt
+++ app/src/main/java/ro/pub/cs/systems/eim/practicaltest01var04/PracticalTest01Var04MainActivity.kt
@@ -1,12 +1,16 @@
 package ro.pub.cs.systems.eim.practicaltest01var04
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.util.Log
 
 class PracticalTest01Var04MainActivity : AppCompatActivity() {
+
+    private lateinit var navigateToSecondaryActivityButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,5 +21,10 @@ class PracticalTest01Var04MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        //navigateToSecondaryActivityButton = findViewById<(R.id.navigate_to_secondary_activity_button)
+        //navigateToSecondaryActivityButton.setOnClickListener {
+            // Acest cod este chemat la apasarea unui buton
+        //}
     }
 }
